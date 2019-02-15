08cefcb0fa00f0ac018ae031ff8d5db5ce991f1e
Kurenkova <47657511+Kurenkova@users.noreply.github.com>
2019-02-15 11:27:22 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,153 @@
+#импорт Flask
+from flask import Flask, request, render_template, url_for
+#импорт коннектора MySQL
+import mysql.connector
+#импорт библиотеки машинного обучения
+from sklearn.cluster import KMeans
+#импорт numpy - библиотеки для работы с массивами
+import numpy as np
+#для "сортировки" словаря
+import operator
+#для рассылки по электронной почте
+from flask_mail import Mail, Message
+
+app = Flask(__name__)
+mail = Mail(app)
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="aic",
+  passwd="pdc",
+  database="aicpdc"
+)
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'kurenkovadsh@gmail.com'
+app.config['MAIL_PASSWORD'] = '*****'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
+
+@app.route('/')
+def hello_world():
+	return render_template('index.html')
+
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+	if request.method == 'POST':
+		#обработка данных с формы
+		result = request.form
+		groups_count = int(result['groups_count'])
+		items_count = int(result['items_count'])
+		date_from = result['date_from']
+		date_to = result['date_to']
+		print(groups_count)
+		print(items_count)
+		print(date_from)
+		print(date_to)
+		#выбор данных для обучения модели
+		mycursor = mydb.cursor()
+		mycursor.execute("SELECT Age, money_month, money_month_dc FROM all_persons")
+		myresult = mycursor.fetchall()
+		all_data = []
+		for r in myresult:
+			row_data = [float(r[0]), float(r[1]), float(r[2])]
+			all_data.append(row_data)
+		X = np.array(all_data)
+		#кластеризация покупателей
+		model = KMeans(n_clusters=groups_count)
+		model.fit(X)
+		clusters = model.predict(X)
+		mycursor.execute("SELECT PInd FROM all_persons")
+		myresult = mycursor.fetchall()
+		persons = []
+		for r in myresult:
+			persons.append(r[0])
+		for p, g in zip(persons, clusters):
+			sql = 'UPDATE all_persons SET PGroup = ' + str(g) + ' WHERE PInd = ' + str(p)
+			mycursor.execute(sql)
+		mydb.commit()
+		mycursor.execute("SELECT PGroup, PInd FROM all_persons ORDER BY PGroup")
+		myresult = mycursor.fetchall()
+		table_data = []
+		table_data.append([])
+		gr = 0
+		for r in myresult:
+			if gr == r[0]:
+				table_data[gr].append(r[1])
+			else:
+				gr = r[0]
+				table_data.append([])
+		#Определяем спрос на товары по группам:
+		sql = """SELECT all_persons.PGroup, goods_shop_check_data.Goods_article, goods_shop_check_data.Goods_name, shop_check_data.BankCardInd, shop_check_data.CardInd
+				FROM all_persons, goods_shop_check_data JOIN shop_check_data 
+				ON goods_shop_check_data.CheckInd = shop_check_data.CheckInd
+				WHERE all_persons.bcId = shop_check_data.BankCardInd OR all_persons.dcId = shop_check_data.CardInd
+				ORDER BY all_persons.PGroup"""
+		mycursor.execute(sql)
+		myresult = mycursor.fetchall()
+		goods_data = []
+		ind = 0
+		i = 0
+		g_dict = {}
+		while i < len(myresult):
+			if (myresult[i][0] == ind):
+				if myresult[i][2] in g_dict:
+					n_v = g_dict[myresult[i][2]] + 1
+					g_dict.update({myresult[i][2]: n_v})
+				else: 
+					g_dict.update({myresult[i][2]: 1})
+				i = i + 1
+			else:
+				goods_data.append(g_dict)
+				ind = myresult[i][0]
+				g_dict = {}
+		goods_data.append(g_dict)
+		#Отбираем товары по количеству (не больше, чем мы указали)
+		goods_lists = []
+		for r in goods_data:
+			goods_list = []
+			#for key in sorted(r, key=r.get, reverse=True):
+			#	goods_list.append(key)
+			sorted_list = sorted(r.items(), key=operator.itemgetter(1), reverse=True)
+			for i in sorted_list:
+				goods_list.append(i[0])
+			#for key in r:
+			#	goods_list.append(key)
+			final_list = goods_list[0:min(len(goods_list), items_count)]
+			goods_lists.append(final_list)
+		#Рассылка по электронной почте
+		mycursor.execute("SELECT PGroup, Email FROM all_persons WHERE Email IS NOT NULL")
+		myresult = mycursor.fetchall()
+		for r in myresult:
+			try:
+				msg = Message('Рекомендации товаров только для Вас!', sender = 'Магазин АИС ПДС', recipients = [str(r[1])])
+				msg.body = "Рекомендуем приобрести следующие товары по эксклюзивным ценам: " + ", ".join(goods_lists[r[0]])
+				#mail.send(msg)
+			except:
+				print('Error sending emial to recipient ' + str(r[1]))
+		#Готовим данные для отображения
+		groups_data = []
+		for i in range(len(table_data)):
+			#obj = {'ind': i, 'g_size': len(table_data[i]), 'goods': ", ".join(goods_data[i])}
+			obj = {'ind': i, 'g_size': len(table_data[i]), 'goods': ", ".join(goods_lists[i])}
+			groups_data.append(obj)
+		return render_template("result.html", persons = table_data, groups_data = groups_data)
+	if request.method == 'GET':
+		mycursor = mydb.cursor()
+		mycursor.execute("SELECT * FROM banks")
+		myresult = mycursor.fetchall()
+		return render_template("result.html", banks = myresult)
+
+
+@app.route('/neznayu')
+def davaitevypridymaete():
+	if request.method == 'GET':
+		uchenick = {'kluch': 'znachenie'}
+		return render_template("result.html", moi_slovar=uchenick)
+
+
+
+
+app.run(debug=True, host='0.0.0.0', port=3000)
---
+++ job.py
@@ -0,0 +1,69 @@
+import mysql.connector
+from decimal import Decimal
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="aic",
+  passwd="pdc",
+  database="aicpdc"
+)
+
+mycursor = mydb.cursor()
+mycursor.execute("DELETE FROM all_persons");
+sql = """SELECT * 
+FROM  
+(SELECT users_discount_cards.PCardInd as dc_PID, users_discount_cards.Age as dc_age, users_discount_cards.email AS dc_email, users_discount_cards.civility AS dc_civility, DATEDIFF(MAX(shop_check_data.Purchase_time), MIN(shop_check_data.Purchase_time)) AS dc_days, AVG(shop_check_data.Purchase_price) AS dc_avg 
+FROM users_discount_cards, shop_check_data 
+WHERE shop_check_data.CardInd IS NOT NULL 
+AND shop_check_data.CardInd = users_discount_cards.CardInd
+GROUP BY dc_PID) dc JOIN
+(SELECT PBankCardInd AS bc_PID, Civility AS bc_civility, Age AS bc_age, Email AS bc_email, Money_month as bc_avg
+FROM users_bank_cards) bc
+ON dc.dc_age = bc.bc_age AND dc.dc_civility = bc.bc_civility AND dc.dc_email = bc.bc_email
+GROUP BY bc.bc_PID;"""
+mycursor.execute(sql)
+myresult = mycursor.fetchall()
+for r in myresult:
+	sql = 'INSERT INTO all_persons(civility, age, email, money_month, money_month_dc, dcId, bcId) VALUES("' + r[3] + '", ' + str(r[1]) + ', "' + r[2] + '", ' + str(r[10]) + ', ' + str(float(r[5])/((float(r[4])+1)/30)) + ', ' + str(r[0]) + ', ' + str(r[6]) + ')'
+	mycursor.execute(sql)
+	mydb.commit()
+#Добавление пользователей, платящих только картой лояльности
+discount_cards = []
+mycursor.execute("SELECT dcId FROM all_persons")
+myresult = mycursor.fetchall()
+for r in myresult:
+	discount_cards.append(r[0])
+sql = """SELECT users_discount_cards.PCardInd as dc_PID, users_discount_cards.Age as dc_age, users_discount_cards.email AS dc_email, users_discount_cards.civility AS dc_civility, DATEDIFF(MAX(shop_check_data.Purchase_time), MIN(shop_check_data.Purchase_time)) AS dc_days, AVG(shop_check_data.Purchase_price) AS dc_avg 
+FROM users_discount_cards, shop_check_data 
+WHERE shop_check_data.CardInd IS NOT NULL 
+AND shop_check_data.CardInd = users_discount_cards.CardInd
+GROUP BY dc_PID"""
+mycursor.execute(sql)
+myresult = mycursor.fetchall()
+for r in myresult:
+	if not (r[0] in discount_cards):
+		try:
+			sql = 'INSERT INTO all_persons(civility, age, email, money_month, money_month_dc, dcId) VALUES("' + r[3] + '", ' + str(r[1]) + ', "' + r[2] + '", ' + str(0) + ', ' + str(float(r[5])/((float(r[4])+1)/30)) + ', ' + str(r[0]) + ')'
+			mycursor.execute(sql)
+		except:
+			print("Incorect database record")
+mydb.commit() 
+#Добавление пользователей, платящих только банковскими картами
+bank_cards = []
+mycursor.execute("SELECT bcId FROM all_persons")
+myresult = mycursor.fetchall()
+for r in myresult:
+	bank_cards.append(r[0])
+sql = """ SELECT PBankCardInd AS bc_PID, Civility AS bc_civility, Age AS bc_age, Email AS bc_email, Money_month as bc_avg
+FROM users_bank_cards """
+mycursor.execute(sql)
+myresult = mycursor.fetchall()
+for r in myresult:
+	if not (r[0] in bank_cards):
+		try:
+			sql = 'INSERT INTO all_persons(civility, age, email, money_month, money_month_dc, bcId) VALUES("' + r[1] + '", ' + str(r[2]) + ', "' + r[3] + '", ' + str(r[4]) + ', ' + str(0) + ', ' + str(r[0]) + ')'
+			mycursor.execute(sql)
+		except:
+			print("Incorect database record")
+mydb.commit() 
+
